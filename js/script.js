d04f42ca106a58794c7ad2d99d2a244986c9c59c
Victor Pereira <victorpereirafernandes20070101@gmail.com>
2025-10-04 13:58:17 -0300

    atualização 4.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 console.log("Nimbus Security Page Loaded ✅");
+// Tela de carregamento
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loading-screen");
+    loader.style.display = "none"; // esconde a tela de carregamento quando terminar de carregar
+});
+
+// Tela offline
+function updateOnlineStatus() {
+    const offlineScreen = document.getElementById("offline-screen");
+    if (navigator.onLine) {
+    offlineScreen.style.display = "none";
+    } else {
+    offlineScreen.style.display = "flex";
+    }
+}
+
+// Detecta mudanças na conexão
+window.addEventListener("online", updateOnlineStatus);
+window.addEventListener("offline", updateOnlineStatus);
+
+// Checa status inicial
+updateOnlineStatus();
+
+
+// Função para revelar elementos ao rolar a página
+function revealOnScroll() {
+    const reveals = document.querySelectorAll(".reveal, .reveal-left, .reveal-right");
+
+    for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 100; // quanto precisa aparecer para ativar
+
+    if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+// Escuta o scroll
+window.addEventListener("scroll", revealOnScroll);
+
+// Executa na primeira carga também
+revealOnScroll();
